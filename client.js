bb6d6e5089b7bb7263611a5519c1329bcf80dc19
Zach Wright <zwright101@gmail.com>
2023-07-19 12:22:28 -0500

    functionality works for submitting fields. need to style table and data better

---
+++ client.js
@@ -0,0 +1,61 @@
+console.log('Hello World!');
+
+let totalSalaries = [];
+let monthlyTotal = 0;
+
+function submitForm(event){
+    event.preventDefault();
+    let firstName = document.getElementById('first').value;
+    let lastName = document.getElementById('last').value;
+    let id = document.getElementById('id').value;
+    let title = document.getElementById('title').value;
+    let salary = Number(document.getElementById('salary').value);
+
+    // let deleteButton = `<button id="delete" onclick="removeRow(${salary})">Delete</button>`;
+    let employees = document.getElementById('employeeList');
+
+    employees.innerHTML += `
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${id}</td>
+            <td>${title}</td>
+            <td>${salary}</td>
+            <td><button onClick="removeRow(event)">Remove</button></td>
+        </tr>
+    `
+
+    clearInputs();
+
+    totalSalaries.push(salary);
+    console.log(totalSalaries);
+    let monthlyTotal = equateMonthly();
+    document.querySelector('#monthly').innerHTML = monthlyTotal;
+    redSalary(monthlyTotal)
+    }
+
+    function clearInputs(){
+        document.getElementById('employee-form').reset();
+    }
+
+    function equateMonthly(){
+        let monthly = 0;
+        for (let i = 0; i < totalSalaries.length; i++){
+            monthly += totalSalaries[i] / 12
+        }
+        console.log(monthly.toFixed(2));
+        return monthly
+    }
+
+    function redSalary(monthly){
+        if (monthly >= 20000){
+            document.querySelector('#monthly').style.backgroundColor = 'red';
+        }
+    }
+    
+
+    function removeRow(event){
+        event.target.closest('tr').remove();
+    }
+    
+
